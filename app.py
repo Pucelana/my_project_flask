6d7216d366299e1f4dcda5282a5ba5a8b35f84e8
Pucela83 <vane_euro@hotmail.com>
2023-06-13 21:29:10 +0200

    nuevos archivos

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os 
-from flask import Flask 
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
 env_config = os.getenv( "PROD_APP_SETTINGS" , "config.DevelopmentConfig" ) 
 app.config.from_object(env_config) 
 
